a02dcd37fa5e61ca253a83e6e10a95817debda4b
avbravikanth1 <107469571+avbravikanth1@users.noreply.github.com>
2022-10-31 15:33:53 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfb27statefile"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "devopsb27-terraform-locking"
-    encrypt        = true
-  }
-}
